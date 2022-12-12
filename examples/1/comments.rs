fa7bd8d4044cc69acbbadfe0d8334a1e5b6cfd28
akinwilson <akinola.antony.wilson@gmail.com>
2022-12-12 19:39:30 +0000

    comments in line of calculation

---
+++ examples/1/comments.rs
@@ -0,0 +1,14 @@
+fn main () {
+    // a line comment 
+    
+    /*println!("comment executable")
+     *block of code commented out 
+    */
+
+    /*
+     You dont need a star on every line
+     */
+    let x = 5 + /*90*/ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+}
